06774e5d3ba03c3f216dbe72b110c0cf1c713264
narkan <naderlorry@yahoo.fr>
2019-07-15 23:13:06 +0200

    dernier changement

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,11 +1,28 @@
 def echo (string)
-	return string 
-end 
-
-def shout (string)
-	return string.upcase
+    return string
 end
 
-def repeat (string, *n)
-	return string *n.join(" ").to_i
-end 
+def shout (string)
+    return string.upcase
+end
+
+def repeat (truc, n = 2, *p)
+    if p != nil
+        return (truc + " ")*(n - 1) + truc
+        else
+        return (truc + " ")*(p - 1) + truc
+    end
+end
+
+def start_of_word(string, n)
+    return string[0..(n-1)]
+end
+
+def first_word (string)
+    return string.split(" ")[0]
+end
+
+def titleize (string)
+    nocaps = "and", "or", "the"
+    return string.capitalize.split(" ").map { |word| nocaps.include?(word) ? word : word.capitalize }.join(" ")
+end
